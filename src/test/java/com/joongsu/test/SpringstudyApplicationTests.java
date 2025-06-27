370a37cf765576615c5f10f4aa7417add797feac
gojungsu <gjs4565@naver.com>
2025-06-27 19:18:24 +0900

    first commit

---
+++ src/test/java/com/joongsu/test/SpringstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joongsu.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
